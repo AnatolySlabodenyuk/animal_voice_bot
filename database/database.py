53bfcbde337a3cf3d8a53016d8033d5675be58f2
Slabodenyuk Anatoly <slabodenyukanatoly@gmail.com>
2024-06-18 12:25:43 +0300

    change database

--- database/database.py
+++ database/database.py
@@ -3,13 +3,13 @@ import aiosqlite
 from contextlib import closing
 
 DATABASE_NAME = "database/file_database.db"
-table_name = "files_information"
+TABLE_NAME = "files_information"
 
 
 async def create_table():
     async with aiosqlite.connect(DATABASE_NAME) as connection:
         await connection.execute(f'''
-            CREATE TABLE IF NOT EXISTS {table_name} (
+            CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             file_name TEXT NOT NULL,
             file_id TEXT NOT NULL
@@ -18,32 +18,42 @@ async def create_table():
         await connection.commit()
 
 
-async def get_file_id_from_table(file_name: str):
+async def get_file_id_from_table(file_name: str) -> str:
     async with aiosqlite.connect(DATABASE_NAME) as connection:
         cursor = await connection.execute(f'''
             SELECT file_id 
-            FROM {table_name} 
+            FROM {TABLE_NAME} 
             WHERE file_name = ?
         ''', (file_name,))
         result = await cursor.fetchone()
         return result
 
 
-async def get_file_name_from_table(id: int):
+async def get_file_name_from_table(id: int) -> str:
     async with aiosqlite.connect(DATABASE_NAME) as connection:
         cursor = await connection.execute(f'''
             SELECT file_name 
-            FROM {table_name} 
+            FROM {TABLE_NAME} 
             WHERE id = ?
         ''', (id,))
         result = await cursor.fetchone()
-        return result
+        return result[0] if result else None
+
+
+async def get_button_ids() -> list[int]:
+    async with aiosqlite.connect(DATABASE_NAME) as connection:
+        cursor = await connection.execute(f'''
+            SELECT id 
+            FROM {TABLE_NAME}
+        ''')
+        result = await cursor.fetchall()
+        return [row[0] for row in result]
 
 
 async def add_audio_to_table(file_name, file_id):
     async with aiosqlite.connect(DATABASE_NAME) as connection:
         await connection.execute(f'''
-            INSERT INTO {table_name} (file_name, file_id)
+            INSERT INTO {TABLE_NAME} (file_name, file_id)
             VALUES (?, ?)
         ''', (file_name, file_id))
         await connection.commit()
