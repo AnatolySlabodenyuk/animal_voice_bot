09285429a886abe8abdfd25468571c1b74101cb8
Anatoly Slabodenyuk <slabodenyukanatoly@gmail.com>
2024-11-24 23:13:35 +0300

    commit from MacBook

--- main.py
+++ main.py
@@ -8,19 +8,17 @@ from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
 from keyboards.set_menu import set_main_menu
 
-
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
 
 
 # Функция конфигурирования и запуска бота
 async def main():
-
     # Конфигурируем логирование
     logging.basicConfig(
         level=logging.INFO,
         format="%(filename)s:%(lineno)d #%(levelname)-8s "
-        "[%(asctime)s] - %(name)s - %(message)s")
+               "[%(asctime)s] - %(name)s - %(message)s")
 
     # Выводим в консоль информацию о начале запуска бота
     logger.info("Starting bot")
